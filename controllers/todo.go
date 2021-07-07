f0ebc724a50072225a2586afb18e83dfa2e61374
Breno Baptista <brenobaptista@protonmail.com>
2021-07-06 22:53:13 -0300

    Added the rest of the endpoints

--- controllers/todo.go
+++ controllers/todo.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/brenobaptista/library-go-fiber/models"
 	"github.com/gofiber/fiber/v2"
 )
@@ -19,5 +22,156 @@ func GetTodos(c *fiber.Ctx) error {
 		"data": fiber.Map{
 			"todos": todos,
 		},
+	})
+}
+
+func GetTodo(c *fiber.Ctx) error {
+	paramId := c.Params("id")
+
+	id, err := strconv.Atoi(paramId)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse Id",
+		})
+	}
+
+	for _, todo := range todos {
+		if todo.Id == id {
+			return c.Status(fiber.StatusOK).JSON(fiber.Map{
+				"success": true,
+				"data": fiber.Map{
+					"todo": todo,
+				},
+			})
+		}
+	}
+
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		"success": false,
+		"message": "Todo not found",
+	})
+}
+
+func CreateTodo(c *fiber.Ctx) error {
+	type Request struct {
+		Title string `json:"title"`
+	}
+
+	var body Request
+
+	err := c.BodyParser(&body)
+
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse JSON",
+		})
+	}
+
+	todo := &models.Todo{
+		Id:        len(todos) + 1,
+		Title:     body.Title,
+		Completed: false,
+	}
+
+	todos = append(todos, todo)
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"todo": todo,
+		},
+	})
+}
+
+func UpdateTodo(c *fiber.Ctx) error {
+	paramId := c.Params("id")
+
+	id, err := strconv.Atoi(paramId)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse id",
+		})
+	}
+
+	type Request struct {
+		Title     *string `json:"title"`
+		Completed *bool   `json:"completed"`
+	}
+
+	var body Request
+	err = c.BodyParser(&body)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse JSON",
+		})
+	}
+
+	var todo *models.Todo
+
+	for _, t := range todos {
+		if t.Id == id {
+			todo = t
+			break
+		}
+	}
+
+	if todo.Id == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"success": false,
+			"message": "Not found",
+		})
+	}
+
+	if body.Title != nil {
+		todo.Title = *body.Title
+	}
+
+	if body.Completed != nil {
+		todo.Completed = *body.Completed
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true,
+		"data": fiber.Map{
+			"todo": todo,
+		},
+	})
+}
+
+func DeleteTodo(c *fiber.Ctx) error {
+	paramId := c.Params("id")
+
+	id, err := strconv.Atoi(paramId)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"success": false,
+			"message": "Cannot parse id",
+		})
+	}
+
+	for i, todo := range todos {
+		if todo.Id == id {
+
+			todos = append(todos[:i], todos[i+1:]...)
+
+			return c.Status(fiber.StatusNoContent).JSON(fiber.Map{
+				"success": true,
+				"message": "Deleted Succesfully",
+			})
+		}
+	}
+
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+		"success": false,
+		"message": "Todo not found",
 	})
 }
