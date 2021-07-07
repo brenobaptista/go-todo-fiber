1b8349a315545ad5ef4a160ae76620c4e87e5525
Breno Baptista <brenobaptista@protonmail.com>
2021-07-06 21:56:06 -0300

    Updated controllers description

--- controllers/Todo.go
+++ controllers/Todo.go
@@ -3,21 +3,21 @@ package controllers
 import "github.com/gofiber/fiber/v2"
 
 func GetAllTodos(c *fiber.Ctx) error {
-	return c.SendString("All Todos")
+	return c.SendString("Got all to-dos")
 }
 
 func GetTodoByID(c *fiber.Ctx) error {
-	return c.SendString("Single Todo")
+	return c.SendString("Got a single to-do")
 }
 
 func CreateTodo(c *fiber.Ctx) error {
-	return c.SendString("New Todo")
+	return c.SendString("Created to-do")
 }
 
 func ToggleTodoStatus(c *fiber.Ctx) error {
-	return c.SendString("Updated Todo")
+	return c.SendString("Updated to-do")
 }
 
 func DeleteTodo(c *fiber.Ctx) error {
-	return c.SendString("Delete Todo")
+	return c.SendString("Deleted to-do")
 }
